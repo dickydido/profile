a1ab1565e18dc03e5a00d93adb7447d279378f77
Richie <richie.withers@hotmail.co.uk>
2019-02-22 23:39:18 +0000

    Added copyright message

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+window.onload = function() {
+//Copyright year.
+var today = new Date();
+var year = document.createTextNode(today.getFullYear());
+var copyright = document.querySelector('#copyright');
+copyright.appendChild(year);
 
+};
